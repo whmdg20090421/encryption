5b4cabcc3945e43ed4d2f21037b36015baabd26d
root <root@localhost.localdomain>
2026-07-13 04:32:25 +0000

    fix: ShellExecutor 添加 Log import

--- APP/core/src/main/java/com/whmdg/mczj/tools/security/ShellExecutor.kt
+++ APP/core/src/main/java/com/whmdg/mczj/tools/security/ShellExecutor.kt
@@ -1,6 +1,7 @@
 package com.whmdg.mczj.tools.security
 
 import android.content.Context
+import android.util.Log
 import com.topjohnwu.superuser.Shell
 import com.whmdg.mczj.tools.AppDataPaths
 import com.whmdg.mczj.tools.util.DiagnosticLog
